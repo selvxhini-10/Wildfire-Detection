8fedb3e6d62118be966839a8efba354f170ede1f
Richard Su <suruiquan10@163.com>
2025-09-13 14:53:38 -0400

    chore: fixed some other issues

--- wildfire-detection/app/page.tsx
+++ wildfire-detection/app/page.tsx
@@ -2,6 +2,11 @@ import CanadianFireMap from "./fire_map";
 
 export default function Home() {
   return (
+    <>
+      <head>
+        <title>Canadian Forest Fire Monitor</title>
+      </head>
       <CanadianFireMap />
+    </>
   );
 }
